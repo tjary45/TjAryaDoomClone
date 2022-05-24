71fcd1d75ab2e23599a6fb437da9824bfb8f4a10
tjary45 <88062393+tjary45@users.noreply.github.com>
2022-05-24 13:24:59 +1200

    ale
    vid 2

---
+++ scenes/player.gd
@@ -0,0 +1,19 @@
+extends KinematicBody
+
+var velocity = Vector3()
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func get_input():
+	pass
+	
+	
+func _unhandled_input(event):
+	pass
+	
+func _physics_process(delta):
+	pass
+	
+func change_gun():
+	
