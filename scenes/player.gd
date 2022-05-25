4b3084c413e0e266a2e3d76013e47288b61f1de2
tjary45 <88062393+tjary45@users.noreply.github.com>
2022-05-25 14:57:29 +1200

    just about don with mouse movement

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,19 +1,59 @@
 extends KinematicBody
 
-var velocity = Vector3()
 
+#basic variables
+var velocity = Vector3()
+var gravity = -30 
+var max_speed = 8
+var mouse_sensitivity = 0.002
+
+#gun variable 
+
+
+
+
+
+
+
+#functions
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 func get_input():
-	pass
-	
+	var input_dir = Vector3()
+	if Input.is_action_pressed("move_forward"):
+		input_dir += -global_transform.basis.z
+	if Input.is_action_pressed("move_back"):
+		input_dir +=  global_transform.basis.z
+	if Input.is_action_pressed("strafe_left"):
+		input_dir += -global_transform.basis.x
+	if Input.is_action_pressed("strafe_right"):
+		input_dir += global_transform.basis.x
+	# input_dur = input_dir.normalize() 
+		
+	return input_dir
 	
 func _unhandled_input(event):
-	pass
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.y * mouse_sensitivity)
+		$Pivot.rotate_x(-event.relative.x * mouse_sensitivity)
+		$Pivot.rotation.x = clamp($Pivot.rotation.x,-1.2,1.2)
 	
 func _physics_process(delta):
-	pass
+	#gravity
+	velocity.y += gravity * delta
+	var desired_velocity = get_input() * max_speed
+	velocity.x = desired_velocity.x
+	velocity.z = desired_velocity.z
+	velocity = move_and_slide(velocity, Vector3.UP, true)
+	
+	
+	
+	
 	
 func change_gun():
+	pass
 	
+func _process(delta):
+	pass
+
