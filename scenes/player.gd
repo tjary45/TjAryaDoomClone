bc83a884b2f78d1d5ed4cafd7be1ea5489deac39
tjary45 <88062393+tjary45@users.noreply.github.com>
2022-06-02 14:55:44 +1200

    shotgun stuck
    do dooo

--- scenes/player.gd
+++ scenes/player.gd
@@ -9,9 +9,10 @@ var mouse_sensitivity = 0.002
 
 
 #gun variable 
-
-
-
+onready var pistol = preload("res://scenes/Pistol.tscn")
+onready var shotgun = preload("res://scenes/Shotgun.tscn")
+var current_gun = 0 
+onready var carried_guns = [pistol.shotgun]
 
 
 
@@ -53,8 +54,18 @@ func _physics_process(delta):
 	
 	
 func change_gun():
-	pass
-	
+	$Pivot/Gun.get_child(0).queue_free()
+	var new_gun = carried_guns[gun].instance()
+	$Pivot/Gun.add_child(new_gun)
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("next_gun"):
+		current_gun=+1
+		if current_gun > len(carried_guns)-1:
+			current_gun = 0
+			change_gun(current_gun)
+	elif Input.is_action_just_pressed("prev_gun"):
+		current_gun -=1
+		if current_gun < 0:
+			current_gun = len(carried_guns)-1
+		change_gun(current_gun)
 
